dedcd4ca361dfd7c092cb82451fce9a50769ebc8
Akino <akinochen@foxmail.com>
2024-11-11 23:14:19 +0800

    chore: remove `nx` and using `semantic-release` to manage release

--- vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-/// <reference types="vite/client" />
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-
-  const component: DefineComponent<any, any, any>
-  export default component
-}
