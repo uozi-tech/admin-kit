018d7611cbcf7d12a1e697f2f42df4caea73b5c6
Akino <akinochen@foxmail.com>
2024-11-18 17:56:15 +0800

    fix(shared-config): cannot find "xxx" type def

--- packages/shared-config/tsup.config.ts
+++ packages/shared-config/tsup.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'tsup'
+import * as fs from 'node:fs'
 
 export default defineConfig({
-  entry: ['vite', 'tsconfig'],
+  entry: ['vite'],
   target: 'esnext',
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   splitting: false,
   sourcemap: false,
   dts: true,
   clean: true,
   external: ['vite', 'unocss'],
-  noExternal: ['./**/*.json'],
+  onSuccess: () => {
+    fs.cpSync('tsconfig/tsconfig.app.json', 'dist/tsconfig.app.json')
+    fs.cpSync('tsconfig/tsconfig.node.json', 'dist/tsconfig.node.json')
+    return undefined
+  },
 })
