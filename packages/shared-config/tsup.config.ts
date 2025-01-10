7ebffd2f2670e355b69056d640f30735aba3fe2e
Akino <akinochen@foxmail.com>
2025-01-10 15:35:50 +0800

    chore: upgrade deps

--- packages/shared-config/tsup.config.ts
+++ packages/shared-config/tsup.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'tsup'
-import * as fs from 'node:fs'
 
 export default defineConfig({
   entry: ['vite'],
@@ -9,10 +8,5 @@ export default defineConfig({
   sourcemap: false,
   dts: true,
   clean: true,
-  external: ['vite', 'unocss'],
-  onSuccess: () => {
-    fs.cpSync('tsconfig/tsconfig.app.json', 'dist/tsconfig.app.json')
-    fs.cpSync('tsconfig/tsconfig.node.json', 'dist/tsconfig.node.json')
-    return undefined
-  },
+  external: ['vite'],
 })
