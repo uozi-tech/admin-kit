ebfda4c1d8f4e9d14ee7e74f5121f5a03f2efdd9
Akino <akinochen@foxmail.com>
2025-01-17 18:20:01 +0800

    refactor(request): change params and typedef

--- playground/src/api/user.ts
+++ playground/src/api/user.ts
@@ -1,3 +1,21 @@
 // import { Curd } from '@uozi-admin/request'
 //
 // export const userApi = new Curd('http://localhost:4523/m1/3871056-3505026-default/users')
+
+import { extendCurdApi, useCurdApi } from '@uozi-admin/request/src/useCurdApi.js'
+
+export const userApi = useCurdApi<{ aa: string }>('/aass')
+
+extendCurdApi(userApi, {
+  getList: async () => {
+    console.log('getList')
+    return {
+      data: [],
+      pagination: {
+        total: 0,
+        page: 1,
+        pageSize: 10,
+      },
+    }
+  },
+})
