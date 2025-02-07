3ea1d3163457b8af91a2c6c62be6ac4c9176cd82
Akino <akinochen@foxmail.com>
2025-02-07 23:15:16 +0800

    feat(curd): add `radio group`

---
+++ packages/curd/src/components/form/StdRadioGroup.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { RadioGroupConfig } from '~curd/types'
+import { RadioGroup } from 'ant-design-vue'
+
+defineProps<{ props?: RadioGroupConfig }>()
+
+const value = defineModel<number>('value')
+</script>
+
+<template>
+  <RadioGroup
+    v-model:value="value"
+    v-bind="props"
+  />
+</template>
+
+<style scoped lang="less">
+
+</style>
