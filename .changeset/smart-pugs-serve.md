ee5359b0e2a8376eed8c4edd74d8ac7ee46c3c12
Akino <akinochen@foxmail.com>
2025-02-08 14:11:49 +0800

    feat(create-uozi-admin): [template] upgrade admin kit

---
+++ .changeset/smart-pugs-serve.md
@@ -0,0 +1,5 @@
+---
+"create-uozi-admin": minor
+---
+
+feat(create-uozi-admin): [template] upgrade admin kit
--- packages/create-uozi-admin/template/src/main.ts
+++ packages/create-uozi-admin/template/src/main.ts
@@ -1,3 +1,4 @@
+import { createCurdConfig } from '@uozi-admin/curd'
 import { createApp } from 'vue'
 import gettext from '~/language/gettext'
 import router from '~/router'
@@ -8,6 +9,16 @@ import '@uozi-admin/curd/dist/index.css'
 import '@uozi-admin/layout-antdv/dist/index.css'
 
 createApp(App)
+  .use(createCurdConfig({
+    listApi: {
+      paginationMap: {
+        total: 'total',
+        current: 'current_page',
+        pageSize: 'per_page',
+        totalPages: 'total_pages',
+      },
+    },
+  }))
   .use(store)
   .use(gettext)
   .use(router)
