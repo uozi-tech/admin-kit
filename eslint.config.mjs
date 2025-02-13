5ed35246f2f9347e4609eaab3c1f2de2e0bc7f37
Akino <akinochen@foxmail.com>
2025-02-13 11:19:43 +0800

    chore: change eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,12 @@ export default antfu({
     '**/.pnpm-store/*',
     '**/locales/**.json',
   ],
+}, {
+  files: ['playground/mock/**/*.ts'],
+  rules: {
+    'no-console': 'off',
+    'ts/no-require-imports': 'off',
+  },
 }).overrideRules({
   'vue/no-dupe-keys': 'off',
   'vue/html-indent': ['error', 2],
@@ -27,13 +33,14 @@ export default antfu({
   }],
   'ts/no-use-before-define': 'off',
   'ts/no-non-null-asserted-optional-chain': 'warn',
+  'ts/explicit-function-return-type': 'off',
+  'ts/no-unsafe-function-type': 'off',
   'no-console': ['warn', { allow: ['error'] }],
   'no-new': 'off',
-  'unused-imports/no-unused-vars': 'warn',
   'jsonc/sort-keys': 'off',
   'node/handle-callback-err': 'off',
-  'ts/no-unsafe-function-type': 'off',
   'node/prefer-global/process': 'off',
   'antfu/no-import-dist': 'off',
+  'unused-imports/no-unused-vars': 'warn',
   'unused-imports/no-unused-imports': 'warn',
 })
