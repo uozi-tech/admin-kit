c7b18b143d568728c824c00502f46454c110b241
Jacky <me@jackyu.cn>
2025-02-13 17:29:37 +0800

    fix(curd): [datepicker] use unix time

--- packages/curd/src/components/form/StdDatePicker.vue
+++ packages/curd/src/components/form/StdDatePicker.vue
@@ -2,22 +2,36 @@
 import type { PanelMode } from 'ant-design-vue/es/vc-picker/interface'
 import type { DatePickerConfig, MonthPickerConfig, WeekPickerConfig } from '../../types'
 import { DatePicker } from 'ant-design-vue'
+import dayjs from 'dayjs'
+import { computed } from 'vue'
 import { Format } from '../../constants'
 
 defineProps<{ props?: (DatePickerConfig | WeekPickerConfig | MonthPickerConfig) & { placeholder?: string | number }, type: PanelMode | 'datetime' }>()
 
-const value = defineModel<DatePickerConfig['value'] | WeekPickerConfig['value'] | MonthPickerConfig['value']>('value')
+const value = defineModel<DatePickerConfig['value'] | WeekPickerConfig['value'] | MonthPickerConfig['value'] | number | string>('value')
+
+const computedValue = computed(() => {
+  if (typeof value.value === 'number') {
+    return dayjs.unix(value.value)
+  }
+  return value.value
+})
+
+function handleChange(v: string) {
+  value.value = dayjs(v).unix()
+}
 </script>
 
 <template>
   <DatePicker
-    v-model:value="value as any"
+    :value="computedValue"
     :picker="type === 'datetime' ? undefined : type as any"
     :format="Format[type]"
     :value-format="Format[type]"
     :show-time="type === 'datetime'"
     :get-popup-container="(triggerNode: any) => triggerNode.parentNode"
     v-bind="props as any"
+    @change="(_, dateString) => handleChange(dateString)"
   />
 </template>
 
