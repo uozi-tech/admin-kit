d21008c43e81739b23c62a8a33b9479494ace9ee
Akino <akinochen@foxmail.com>
2025-02-23 12:54:23 +0800

    feat(layout): support setting header extra gap by user

---
+++ .changeset/rich-grapes-dress.md
@@ -0,0 +1,5 @@
+---
+"@uozi-admin/layout-antdv": minor
+---
+
+feat(layout): support setting header extra gap by user
--- packages/layout-antdv/src/components/AppHeader.vue
+++ packages/layout-antdv/src/components/AppHeader.vue
@@ -4,8 +4,10 @@
 <template>
   <div class="app-header">
     <div />
-    <div class="flex items-center gap-6">
-      <slot name="default" />
+    <div class="flex items-center">
+      <div class="flex items-center gap-4">
+        <slot name="default" />
+      </div>
 
       <!-- 自定义 Header 的插槽内容，默认会显示传入的 slot -->
       <template v-if="$slots.actions">
