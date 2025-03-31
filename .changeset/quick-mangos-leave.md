857b38643475e16d0c76b42c6913e1342fb15f5f
Jacky <me@jackyu.cn>
2025-03-31 11:28:44 +0800

    fix(SidebarContent): adjust margin class for header title

---
+++ .changeset/quick-mangos-leave.md
@@ -0,0 +1,5 @@
+---
+"@uozi-admin/layout-antdv": patch
+---
+
+fix(SidebarContent): adjust margin class for header title
--- packages/layout-antdv/src/components/SidebarContent.vue
+++ packages/layout-antdv/src/components/SidebarContent.vue
@@ -54,7 +54,7 @@ function handleMenuItemClick({ item }) {
         </div>
         <h1
           v-show="!collapsed"
-          class="text-20px font-400 transition-all line-clamp-1 mb-0"
+          class="text-20px font-400 transition-all line-clamp-1 my-0"
         >
           {{ getRealTitle(headerTitle) }}
         </h1>
