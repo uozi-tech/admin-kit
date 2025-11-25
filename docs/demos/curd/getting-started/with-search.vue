972ee4def7d4078936ec148159b5c499c8ed8c9e
Akino <akinochen@foxmail.com>
2025-11-25 11:56:15 +0800

    docs(curd): add actions column to the search demo table for enhanced functionality

--- docs/demos/curd/getting-started/with-search.vue
+++ docs/demos/curd/getting-started/with-search.vue
@@ -24,6 +24,7 @@ const columns: StdTableColumn[] = [
   },
   { title: '邮箱', dataIndex: 'email' },
   { title: '创建时间', dataIndex: 'created_at' },
+  { title: '操作', dataIndex: 'actions' },
 ]
 </script>
 
